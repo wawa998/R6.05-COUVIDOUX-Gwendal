388b597ace4f2da6969ac01e24052e9132147a75
Devin Ivy <devin@bigroomstudios.com>
2021-04-11 23:24:33 -0400

    (flavor) objection v3.0.0

--- server/manifest.js
+++ server/manifest.js
@@ -3,6 +3,7 @@
 const Dotenv = require('dotenv');
 const Confidence = require('@hapipal/confidence');
 const Toys = require('@hapipal/toys');
+const Schwifty = require('@hapipal/schwifty');
 
 // Pull .env into process.env
 Dotenv.config({ path: `${__dirname}/.env` });
@@ -34,7 +35,27 @@ module.exports = new Confidence.Store({
                 options: {}
             },
             {
-                plugin: './plugins/swagger'
+                plugin: '@hapipal/schwifty',
+                options: {
+                    $filter: 'NODE_ENV',
+                    $default: {},
+                    $base: {
+                        migrateOnStart: true,
+                        knex: {
+                            client: 'sqlite3',
+                            useNullAsDefault: true,     // Suggested for sqlite3
+                            connection: {
+                                filename: ':memory:'
+                            },
+                            migrations: {
+                                stub: Schwifty.migrationsStubPath
+                            }
+                        }
+                    },
+                    production: {
+                        migrateOnStart: false
+                    }
+                }
             },
             {
                 plugin: {
